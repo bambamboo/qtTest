c6bb870abab194a3f2c2ce4f337cb6fb7a19f8d6
bambamboo <warm12pm@gmail.com>
2021-02-05 23:06:37 +0700

    set all new

--- mainwindow.py
+++ mainwindow.py
@@ -9,6 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QWidget,QDesktopWidget,QApplication
 import time
 import sqlite3
 from sqlite3 import Error
@@ -26,10 +27,10 @@ class Ui_MainWindow(object):
                 for column_number, data in enumerate(row_data):
                     self.tableWidget.setItem(
                         row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
-            print(str(time.strftime("%H:%M : ", time.localtime())) +
+            print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                   'Load data from database success.')
         except(Error) as e:
-            print(str(time.strftime("%H:%M : ", time.localtime())) +
+            print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                   'ERROR :' + str(e))
         conn.close()
         self.lineEdit.setText('')
@@ -40,56 +41,94 @@ class Ui_MainWindow(object):
         conn = sqlite3.Connection('ex1')
         q_number = self.lineEdit.text()
         named_tuple = time.localtime()  # get struct_time
-        q_enter_time = time.strftime("%H:%M", named_tuple)
+        q_enter_time = time.strftime("%H:%M:%S", named_tuple)
         if q_number != '':
-            #print('q_number != NULL')
-            try:
-                query = 'INSERT INTO queue(q_number,q_enter_time) VALUES(?,?)'
-                cursor = conn.cursor()
-                cursor.execute(query, [q_number, q_enter_time])
-                conn.commit()
-                print(str(time.strftime("%H:%M : ", time.localtime())) +
-                      'Data saved to database success.')
-                self.openDialog('บันทึกข้อมูลเแล้ว')
-            except(Error) as e:
-                print(str(time.strftime("%H:%M : ", time.localtime())) +
-                      'ERROR :' + str(e))
+            temp = self.cfDuplicate(q_number)
+            if temp == False:
+                try:
+                    query = 'INSERT INTO queue(q_number,q_enter_time) VALUES(?,?)'
+                    cursor = conn.cursor()
+                    cursor.execute(query, [q_number, q_enter_time])
+                    conn.commit()
+                    print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+                        'Data saved to database success.')
+                    self.openDialog('บันทึกข้อมูลเแล้ว')
+                except(Error) as e:
+                    print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+                        'ERROR :' + str(e))
+            else:
+                msg = QtWidgets.QMessageBox()
+                msg.setIcon(QtWidgets.QMessageBox.Question)
+                msg.setText("หมายเลข : " + q_number + " ถูกเพิ่มในตารางแล้ว ต้องการจะเรียกเลยหรือไม่")
+                msg.setWindowTitle("แจ้งเตือน")
+                msg.setStandardButtons(QtWidgets.QMessageBox.Ok |
+                                       QtWidgets.QMessageBox.Cancel)
+                retval = msg.exec_()
         else:
-            print(str(time.strftime("%H:%M : ", time.localtime())) +
+            print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                   'Got Null value on LineEdit ,Unable to write Null in database.')
-            self.openDialog('ไม่สามารถบันทึกข้อมูลว่างได้')
+            self.openDialog('ไม่สามารถบันทึกหมายเลขว่างได้')
         conn.close()
         self.loadData()
+
+    def cfDuplicate(self, q_number):
+        totleRow = self.tableWidget.rowCount()
+        foundStatus = False
+        for i in range(totleRow):
+            if self.tableWidget.item(i, 1).text() == q_number:
+                foundStatus = True
+                break
+            else:
+                foundStatus = False  
+        return(foundStatus)
 
     def deleteData(self):
         conn = sqlite3.Connection('ex1')
         cPosition = 1
         rPosition = (int(self.tableWidget.currentRow()))
         if rPosition >= 0:
+            q_number = self.tableWidget.item(rPosition, cPosition).text()
             try:
-                q_number = self.tableWidget.item(rPosition, cPosition).text()
-                query = 'DELETE FROM queue where q_number = ?'
-                cursor = conn.cursor()
-                cursor.execute(query, [q_number])
-                conn.commit()
-                print(str(time.strftime("%H:%M : ", time.localtime())) +
-                    'Delete data from database success.')
-                self.openDialog('ลบข้อมูลเแล้ว')
+                cfCode = self.cfDelete(q_number)
+                if cfCode == 1024:
+                    query = 'DELETE FROM queue where q_number = ?'
+                    cursor = conn.cursor()
+                    cursor.execute(query, [q_number])
+                    conn.commit()
+                    print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+                        'Delete data from database success.')
+                    self.openDialog('ลบหมายเลขเแล้ว')
+                else:
+                    pass
             except(Error) as e:
-                print(str(time.strftime("%H:%M : ", time.localtime())) +
+                print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                     'ERROR :' + str(e))
         else:
-            print(str(time.strftime("%H:%M : ", time.localtime())) +
+            print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                   'Unable to delete Null in database.')
-            self.openDialog('ไม่สามารถลบข้อมูลว่างได้')
+            self.openDialog('ไม่สามารถลบหมายเลขว่างได้')
         conn.close()
         self.loadData()
+
+    def cfDelete(self, q_number):
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Critical)
+        msg.setText("ยืนยันที่จะลบหมายเลข : " + q_number)
+        #msg.setInformativeText("ยืนยันที่จะลบหมายเลข : " + q_number)
+        msg.setWindowTitle("แจ้งเตือน")
+        #msg.setDetailedText("The details are as follows:")
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok |
+                               QtWidgets.QMessageBox.Cancel)
+        retval = msg.exec_()
+        print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+              "value of pressed message box button:", retval)
+        return(retval)
 
     def searchData(self):
         temp = self.lineEdit.text()
         totleRow = self.tableWidget.rowCount()
         foundStatus = False
-        print(foundStatus)
+        #print(foundStatus)
         if temp != '':
             try:
                 for i in range(totleRow):
@@ -101,25 +140,29 @@ class Ui_MainWindow(object):
                 if foundStatus == True:
                     self.tableWidget.selectRow(i)
                 else:
-                    print(str(time.strftime("%H:%M : ", time.localtime())) +'Can not find number in table.')
+                    print(str(time.strftime("%H:%M:%S : ", time.localtime())
+                              ) + 'Can not find number in table.')
                     self.openDialog('ไม่พบหมายเลขที่ต้องการ')
             except(Error) as e:
-                print(str(time.strftime("%H:%M : ", time.localtime())) +
+                print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                         'ERROR :' + str(e))
         else:
-            print(str(time.strftime("%H:%M : ", time.localtime())) +
+            print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
                   'Got Null value on LineEdit ,Unable search Null in table.')
-            self.openDialog('ไม่สามารถค้นข้อมูลว่างได้')
+            self.openDialog('ไม่สามารถค้นหมายเลขว่างได้')
 
+    def getCellPosition(self):
+        pass
+        
     def printInfo(self):
         cPosition = (int(self.tableWidget.currentColumn()))
         rPosition = (int(self.tableWidget.currentRow()))
-        print(str(time.strftime("%H:%M : ", time.localtime())) +
+        print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
               'Row position : ' + str(rPosition))
-        print(str(time.strftime("%H:%M : ", time.localtime())) +
+        print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
               'Column position : ' + str(cPosition))
         temp = self.tableWidget.item(rPosition,cPosition).text()
-        print(str(time.strftime("%H:%M : ", time.localtime())) +
+        print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
               'Data from cell : ' + temp)
 
     def paintTable(self):
@@ -137,16 +180,52 @@ class Ui_MainWindow(object):
         msg = QtWidgets.QMessageBox()
         msg.setWindowTitle('แจ้งเตือน')
         msg.setIcon(QtWidgets.QMessageBox.Information)
-        msg.setText(message + str(time.strftime(" @ %H:%M", time.localtime())))
+        msg.setText(message + str(time.strftime(" @ %H:%M:%S ", time.localtime())))
         msg.exec_()
-      
+
+    def refreshTable(self):
+        conn = sqlite3.Connection('ex1')
+        try:
+            cfCode = self.cfRefresh()
+            if cfCode == 1024:
+                query = 'DELETE FROM queue'
+                cursor = conn.cursor()
+                cursor.execute(query)
+                conn.commit()
+                print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+                    'Refresh(TRUNCATE) queue table in database success.')
+                self.openDialog('เคลียร์ตารางเรียบร้อยแล้ว')
+            else:
+                pass
+        except(Error) as e:
+            print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+                  'ERROR :' + str(e))
+        conn.close()
+        self.loadData()
+
+    def cfRefresh(self):
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Critical)
+        msg.setText("ยืนยันที่จะเคลียร์ตาราง")
+        #msg.setInformativeText("ยืนยันที่จะลบหมายเลข : " + q_number)
+        msg.setWindowTitle("แจ้งเตือน")
+        #msg.setDetailedText("The details are as follows:")
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok |
+                               QtWidgets.QMessageBox.Cancel)
+        retval = msg.exec_()
+        print(str(time.strftime("%H:%M:%S : ", time.localtime())) +
+              "value of pressed message box button:", retval)
+        return(retval)
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(600, 500)
+        MainWindow.resize(700, 500)
+        MainWindow.setMinimumSize(QtCore.QSize(700, 500))
+        MainWindow.setMaximumSize(QtCore.QSize(700, 500))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.btn_call = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_call.setGeometry(QtCore.QRect(500, 380, 89, 71))
+        self.btn_call.setGeometry(QtCore.QRect(558, 380, 131, 71))
         self.btn_call.setObjectName("btn_call")
         self.btn_call.setStyleSheet('background-color: green')
         self.btn_add = QtWidgets.QPushButton(self.centralwidget)
@@ -158,21 +237,21 @@ class Ui_MainWindow(object):
         self.btn_delete.setGeometry(QtCore.QRect(120, 380, 89, 71))
         self.btn_delete.setObjectName("btn_delete")
         self.btn_delete.setStyleSheet('background-color: red')
-        self.btn_load = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_load.setGeometry(QtCore.QRect(230, 380, 89, 71))
-        self.btn_load.setObjectName("btn_load")
-        self.btn_load.setStyleSheet('background-color: blue')
+        self.btn_search = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_search.setGeometry(QtCore.QRect(230, 380, 89, 71))
+        self.btn_search.setObjectName("btn_search")
+        self.btn_search.setStyleSheet('background-color: blue')
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(10, 10, 581, 311))
+        self.tableWidget.setGeometry(QtCore.QRect(10, 10, 681, 311))
         self.tableWidget.setRowCount(10)
         self.tableWidget.setColumnCount(6)
         self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setSortingEnabled(True)
-        self.tableWidget.setCurrentCell(0, 1)
+        self.tableWidget.setSortingEnabled(False)
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setColumnWidth(1,200)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, item)
-        #self.tableWidget.setColumnHidden(0,True)
+        self.tableWidget.setColumnHidden(0,True)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, item)
         item = QtWidgets.QTableWidgetItem()
@@ -189,6 +268,9 @@ class Ui_MainWindow(object):
         font.setPointSize(12)
         self.lineEdit.setFont(font)
         self.lineEdit.setObjectName("lineEdit")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(335, 334, 121, 31))
+        self.comboBox.setObjectName("comboBox")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 22))
@@ -199,14 +281,31 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        # self.action_Invite_window = QtWidgets.QAction(MainWindow)
+        # self.action_Invite_window.setObjectName("action_Invite_window")
+        # self.actionPlace_windows = QtWidgets.QAction(MainWindow)
+        # self.actionPlace_windows.setObjectName("actionPlace_windows")
+        # self.actionini_py = QtWidgets.QAction(MainWindow)
+        # self.actionini_py.setObjectName("actionini_py")
+        # self.menuSetting.addAction(self.action_Invite_window)
+        # self.menuSetting.addAction(self.actionPlace_windows)
+        # self.menuSetting.addAction(self.actionini_py)
+        # self.menubar.addAction(self.menuSetting.menuAction())
         self.action_Invite_window = QtWidgets.QAction(MainWindow)
         self.action_Invite_window.setObjectName("action_Invite_window")
         self.actionPlace_windows = QtWidgets.QAction(MainWindow)
         self.actionPlace_windows.setObjectName("actionPlace_windows")
         self.actionini_py = QtWidgets.QAction(MainWindow)
         self.actionini_py.setObjectName("actionini_py")
+        self.actionDestination = QtWidgets.QAction(MainWindow)
+        self.actionDestination.setObjectName("actionDestination")
+        self.actionDrop_tablel = QtWidgets.QAction(MainWindow)
+        self.actionDrop_tablel.setObjectName("actionDrop_tablel")
         self.menuSetting.addAction(self.action_Invite_window)
         self.menuSetting.addAction(self.actionPlace_windows)
+        self.menuSetting.addAction(self.actionDestination)
+        self.menuSetting.addAction(self.actionDrop_tablel)
+        self.menuSetting.addSeparator()
         self.menuSetting.addAction(self.actionini_py)
         self.menubar.addAction(self.menuSetting.menuAction())
 
@@ -217,12 +316,11 @@ class Ui_MainWindow(object):
         self.loadData()
         self.btn_add.clicked.connect(self.writeData)
         self.btn_delete.clicked.connect(self.deleteData)
-        self.btn_load.clicked.connect(self.searchData)
+        self.btn_search.clicked.connect(self.searchData)
+        self.actionDrop_tablel.triggered.connect(self.refreshTable)
         self.tableWidget.cellDoubleClicked.connect(self.printInfo)
         #self.tableWidget.sortByColumn()
         
-        
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
@@ -232,8 +330,8 @@ class Ui_MainWindow(object):
         self.btn_add.setText(_translate("MainWindow", "M-ADD"))
         self.btn_delete.setWhatsThis(_translate("MainWindow", "<html><head/><body><p>คลิ๊กเพื่อลบคิวที่ป้อนค่าผิดออกจากตารางคิว</p></body></html>"))
         self.btn_delete.setText(_translate("MainWindow", "DELETE"))
-        self.btn_load.setWhatsThis(_translate("MainWindow", "<html><head/><body><p>คลิ๊กเพื่อโหลดตารางคิวใหม่</p></body></html>"))
-        self.btn_load.setText(_translate("MainWindow", "SEARCH"))
+        self.btn_search.setWhatsThis(_translate("MainWindow", "<html><head/><body><p>คลิ๊กเพื่อโหลดตารางคิวใหม่</p></body></html>"))
+        self.btn_search.setText(_translate("MainWindow", "SEARCH"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "id"))
         item = self.tableWidget.horizontalHeaderItem(1)
@@ -246,11 +344,19 @@ class Ui_MainWindow(object):
         item.setText(_translate("MainWindow", "Status"))
         item = self.tableWidget.horizontalHeaderItem(5)
         item.setText(_translate("MainWindow", "Destination"))
+        # self.menuSetting.setTitle(_translate("MainWindow", "Setting"))
+        # self.action_Invite_window.setText(_translate("MainWindow", "Invite setting"))
+        # self.actionPlace_windows.setText(_translate("MainWindow", "Place setting"))
+        # self.actionini_py.setText(_translate("MainWindow", "ini.py"))
         self.menuSetting.setTitle(_translate("MainWindow", "Setting"))
-        self.action_Invite_window.setText(_translate("MainWindow", "Invite setting"))
-        self.actionPlace_windows.setText(_translate("MainWindow", "Place setting"))
+        self.action_Invite_window.setText(
+            _translate("MainWindow", "Invite sentence"))
+        self.actionPlace_windows.setText(
+            _translate("MainWindow", "Place sentence"))
         self.actionini_py.setText(_translate("MainWindow", "ini.py"))
-
+        self.actionDestination.setText(
+            _translate("MainWindow", "Destination setting"))
+        self.actionDrop_tablel.setText(_translate("MainWindow", "Clear tablel"))
 
 if __name__ == "__main__":
     import sys
@@ -258,5 +364,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    
     MainWindow.show()
+    
     sys.exit(app.exec_())
