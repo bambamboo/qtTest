49fd2ae83548359277f674409886079c68bda03b
bambamboo <warm12pm@gmail.com>
2021-02-08 09:57:23 +0700

    add qtable demo

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import sys
 
-from PyQt5.QtSql import QSqlDatabase
-from PyQt5.QtWidgets import QApplication, QMessageBox, QLabel
+from PyQt5.QtCore import Qt
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QMessageBox,
+    QTableView,
+)
 
-# Create the connection
-con = QSqlDatabase.addDatabase("QSQLITE")
-con.setDatabaseName("/home/contacts.sqlite")
+#ติดต่อฐานข้อมูล#
+class createConnection():
 
-# Create the application
+    con = QSqlDatabase.addDatabase("QSQLITE")
+    con.setDatabaseName("ex1")
+    con.open()
+    print('After OPEN command isOpen : ' + str(con.isOpen()))
+    query = QSqlQuery()
+    query.exec('SELECT * FROM queue')
+    query.finish()
+    con.close()
+    con.removeDatabase("ex1")
+
+
+class Contacts(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("QTableView Example")
+        self.resize(415, 200)
+        # Set up the model
+        self.model = QSqlTableModel(self)
+        self.model.setTable("contacts")
+        self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+        self.model.setHeaderData(0, Qt.Horizontal, "q_name")
+        self.model.setHeaderData(1, Qt.Horizontal, "q_time_enter")
+        self.model.setHeaderData(2, Qt.Horizontal, "q_time_call")
+        self.model.setHeaderData(3, Qt.Horizontal, "q_status")
+        self.model.setHeaderData(4, Qt.Horizontal, "q_destination")
+
+        self.model.select()
+        # Set up the view
+        self.view = QTableView()
+        self.view.setModel(self.model)
+        self.view.resizeColumnsToContents()
+        self.setCentralWidget(self.view)
+
 app = QApplication(sys.argv)
-
-# Try to open the connection and handle possible errors
-if not con.open():
-    QMessageBox.critical(
-        None,
-        "App Name - Error!",
-        "Database Error: %s" % con.lastError().databaseText(),
-    )
+if not createConnection():
     sys.exit(1)
-
-# Create the application's window
-win = QLabel("Connection Successfully Opened!")
-win.setWindowTitle("App Name")
-win.resize(200, 100)
+win = Contacts()
 win.show()
 sys.exit(app.exec_())
