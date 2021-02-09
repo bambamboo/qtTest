9c158eb3cd016bb16de6f6d1dedb2a0b1cc165c9
bambamboo <warm12pm@gmail.com>
2021-02-09 17:01:12 +0700

    9/02/2021

--- qtable_demo.py
+++ qtable_demo.py
@@ -1,9 +1,9 @@
 import sys
 
 from PyQt5.QtCore import QAbstractTableModel, Qt
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlQueryModel, QSqlTableModel
 from PyQt5.QtWidgets import (
-    QAbstractItemView, QApplication, QHeaderView,
+    QAbstractItemView, QApplication, QComboBox, QHeaderView,
     QMainWindow,
     QMessageBox,
     QTableView,
@@ -14,6 +14,7 @@ class Contacts(QMainWindow):
         super().__init__(parent)
         self.setWindowTitle("QTableView Example")
         self.resize(800, 600)
+
         # Set up the model
         self.model = QSqlTableModel(self)
         self.model.setTable("queue")
@@ -22,11 +23,10 @@ class Contacts(QMainWindow):
         self.model.setHeaderData(1, Qt.Horizontal, "Number")
         self.model.setHeaderData(2, Qt.Horizontal, "Enter Time")
         self.model.setHeaderData(3, Qt.Horizontal, "Call Time")
-        self.model.setHeaderData(4, Qt.Horizontal, "Status")
-        
+        self.model.setHeaderData(4, Qt.Horizontal, "Status")       
         self.model.setHeaderData(5, Qt.Horizontal, "Destination")
-
         self.model.select()
+
         # Set up the view
         self.view = QTableView()
         self.view.setModel(self.model)
@@ -35,10 +35,26 @@ class Contacts(QMainWindow):
         #self.view.setEditTriggers(QAbstractItemView())
         #self.headerView = QHeaderView()
         #self.view.setHorizontalHeader()
-        #print(str(self.view.editTriggers()))
-        
+        #print(str(self.view.editTriggers()))       
         self.view.resizeColumnsToContents()
+
+        i = self.view.model().index(1,5)
+
+        # self.cbxModel = QSqlQueryModel(self)
+        # self.cbxModel.setQuery = "SELECT des_name FROM destination"
+        # self.cbxModel.query()
+        self.cbxModel = QSqlTableModel(self)
+        self.cbxModel.setTable("destination")
+        self.cbxModel.select()
+
+        self.cbxView = QComboBox()
+        self.cbxView.setModel(self.cbxModel)
+        # เลือก column ที่จะมาแสดง
+        self.cbxView.setModelColumn(1)
+        
+        self.view.setIndexWidget(i,self.cbxView)
         self.setCentralWidget(self.view)
+
 
 def createConnection():
     con = QSqlDatabase.addDatabase("QSQLITE")
@@ -51,6 +67,7 @@ def createConnection():
         )
         return False
     return True
+
 
 app = QApplication(sys.argv)
 if not createConnection():
